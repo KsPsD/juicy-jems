9aecfc0cba14051e936bdaf39ca43892c74c6287
realbro <realbro.noh@gmail.com>
2024-03-30 20:53:34 +0900

    remove redundant code

--- client/src/walletConnect.tsx
+++ client/src/walletConnect.tsx
@@ -1,13 +1,9 @@
-import React, { useEffect,useState } from "react";
-import { createWeb3Modal ,useWeb3Modal} from "@web3modal/wagmi/react";
+import React from "react";
+import { createWeb3Modal, useWeb3Modal } from "@web3modal/wagmi/react";
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-import { WagmiProvider ,useBalance} from "wagmi";
+import { WagmiProvider } from "wagmi";
 import { arbitrum, mainnet } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BigNumberish} from "ethers";
-import { useAccount, useConnect, useDisconnect } from "wagmi";
-
-
 
 const queryClient = new QueryClient();
 
@@ -46,48 +42,16 @@ export const WalletConnectProvider: React.FC<{ children: React.ReactNode }> = ({
 };
 
 const WalletConnect: React.FC = () => {
-  const { isConnected, address } = useAccount();
-  const { connect, connectors } = useConnect();
-  const { disconnect } = useDisconnect();
-  const { data: balance } = useBalance({ address });
-
-  const [userAccount, setUserAccount] = useState<{ address?: `0x${string}`; balance?: BigNumberish }>({});
-  
-
-  const { open } = useWeb3Modal()
-
-
-  const connectWallet = async () => {
-    try {
-    connect({ connector: connectors[0] })
-      
-      console.log("Connected wallet:", connectors);
-    } catch (error) {
-      console.error("Failed to connect wallet:", error);
-    }
-  };
-
-  const disconnectWallet = () => {
-    disconnect();
-    setUserAccount({}); 
-  };
-
-  useEffect(() => {
-    if (isConnected && address && balance) {
-      setUserAccount({ address, balance: balance.value }); 
-      console.log(address, balance)
-
-    } else {
-      setUserAccount({});
-    }
-  }, [isConnected, address, balance]);
+  const { open } = useWeb3Modal();
 
   return (
     <div>
       <>
-      <button onClick={() => open()}>Open Connect Modal</button>
-      <button onClick={() => open({ view: 'Networks' })}>Open Network Modal</button>
-    </>
+        <button onClick={() => open()}>Open Connect Modal</button>
+        <button onClick={() => open({ view: "Networks" })}>
+          Open Network Modal
+        </button>
+      </>
     </div>
   );
 };
