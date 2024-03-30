b9b837df4e5bb43846ef83e240b153d0973d217a
minwoo <minwoo9621@gmail.com>
2024-03-30 15:28:32 +0900

    Modify sidebar to adding button

--- client/src/sidebar.tsx
+++ client/src/sidebar.tsx
@@ -1,48 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Drawer,
   List,
   ListItem,
   ListItemIcon,
   ListItemText,
+  IconButton,
 } from '@mui/material';
 import {
   Home as HomeIcon,
   Info as InfoIcon,
   AttachMoney as InvestIcon,
+  Menu as MenuIcon,
 } from '@mui/icons-material';
 
-export const Sidebar = () => {
+export const Sidebar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setIsOpen(open);
+  };
+
   return (
-    <Drawer
-      variant="permanent"
-      sx={{
-        width: 240,
-        flexShrink: 0,
-        [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
-      }}
-    >
-      <List>
-        <ListItem button component="a" href="/">
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText primary="Home" />
-        </ListItem>
-        <ListItem button component="a" href="/about">
-          <ListItemIcon>
-            <InfoIcon />
-          </ListItemIcon>
-          <ListItemText primary="About" />
-        </ListItem>
-        <ListItem button component="a" href="/invest">
-          <ListItemIcon>
-            <InvestIcon />
-          </ListItemIcon>
-          <ListItemText primary="Invest" />
-        </ListItem>
-      </List>
-    </Drawer>
+    <div>
+      <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
+        <MenuIcon />
+      </IconButton>
+      <Drawer
+        anchor="left"
+        open={isOpen}
+        onClose={toggleDrawer(false)}
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
+        }}
+      >
+        <List>
+          <ListItem button component="a" href="/">
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem button component="a" href="/about">
+            <ListItemIcon>
+              <InfoIcon />
+            </ListItemIcon>
+            <ListItemText primary="About" />
+          </ListItem>
+          <ListItem button component="a" href="/invest">
+            <ListItemIcon>
+              <InvestIcon />
+            </ListItemIcon>
+            <ListItemText primary="Invest" />
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
   );
 };
 
